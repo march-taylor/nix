5f91eca73c19d9f9bc41136a1a4a138089c8d2c9
March <mart.core.v3@gmail.com>
2026-07-24 12:24:11 +0300

    feat: add central host settings

---
+++ settings.nix
@@ -0,0 +1,9 @@
+{
+  username = "march";
+  fullName = "March Taylor";
+  hostname = "nixos";
+  system = "x86_64-linux";
+  stateVersion = "26.05";
+  timezone = "Europe/Berlin";
+  locale = "en_US.UTF-8";
+}
