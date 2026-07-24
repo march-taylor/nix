0f8fc2c1a5163a8e4ea63fa32a6c7d918fa9e91e
March <mart.core.v3@gmail.com>
2026-07-24 13:05:20 +0300

    fix: align user settings with current machine

--- settings.nix
+++ settings.nix
@@ -1,9 +1,9 @@
 {
   username = "mart";
-  fullName = "mart";
+  fullName = "Mart";
   hostname = "nix";
   system = "x86_64-linux";
   stateVersion = "26.05";
-  timezone = "Europe/Moscow";
+  timezone = "Europe/Berlin";
   locale = "en_US.UTF-8";
 }
