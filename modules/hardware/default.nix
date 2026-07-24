f3f61143243f2cec7b54fc716fc635e57ad0f6ae
March <mart.core.v3@gmail.com>
2026-07-24 12:25:19 +0300

    feat: add common hardware services

---
+++ modules/hardware/default.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+{
+  hardware.enableRedistributableFirmware = true;
+  hardware.graphics.enable = true;
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+  services.blueman.enable = true;
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  services.fwupd.enable = true;
+  services.upower.enable = true;
+  services.power-profiles-daemon.enable = true;
+}
