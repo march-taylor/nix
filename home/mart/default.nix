df0be460ad014489e1df1468d62731cc0960d7cf
March <mart.core.v3@gmail.com>
2026-07-24 13:03:05 +0300

    feat: add Home Manager profile for mart

---
+++ home/mart/default.nix
@@ -0,0 +1,22 @@
+{ settings, ... }:
+{
+  imports = [
+    ./programs.nix
+    ./discord.nix
+    ./niri.nix
+  ];
+
+  home.username = settings.username;
+  home.homeDirectory = "/home/${settings.username}";
+  home.stateVersion = settings.stateVersion;
+
+  programs.home-manager.enable = true;
+  xdg.enable = true;
+
+  home.sessionVariables = {
+    BROWSER = "zen";
+    TERMINAL = "kitty";
+    NIXOS_OZONE_WL = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+  };
+}
