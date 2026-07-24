4b0a567602ac5bd87e7ef8482edd1b13f0cb7561
March <mart.core.v3@gmail.com>
2026-07-24 13:00:26 +0300

    feat: enable declarative storage and gaming modules

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,9 +1,11 @@
 { ... }:
 {
   imports = [
+    ./disko.nix
     ./hardware-configuration.nix
     ../../modules/core
     ../../modules/hardware
     ../../modules/desktop
+    ../../modules/gaming
   ];
 }
