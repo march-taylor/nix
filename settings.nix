a3ce8ee72be6017111ddea347b73a2596616334c
March <mart.core.v3@gmail.com>
2026-07-24 12:37:22 +0300

    Update user settings in settings.nix

--- settings.nix
+++ settings.nix
@@ -1,9 +1,9 @@
 {
-  username = "march";
-  fullName = "March Taylor";
-  hostname = "nixos";
+  username = "mart";
+  fullName = "mart";
+  hostname = "nix";
   system = "x86_64-linux";
   stateVersion = "26.05";
-  timezone = "Europe/Berlin";
+  timezone = "Europe/Moscow";
   locale = "en_US.UTF-8";
 }
